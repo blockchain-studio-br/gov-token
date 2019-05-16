0d67edec7dfd120ecb7f77ad88f287b8ceedb7ae
Fabiano <fabiano.paula.martins@gmail.com>
2019-05-15 21:37:41 -0300

    Fixing Profile Header

--- universal-login-example/src/views/ProfileHeader.js
+++ universal-login-example/src/views/ProfileHeader.js
@@ -2,20 +2,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Blockies from 'react-blockies';
 
-const ProfileHeader = (props) => (
-  <div className="row align-items-center">
-    <Blockies seed={props.address.toLowerCase()} size={8} scale={6} />
-    <div>
-      <p className="user-id user-id-header">{props.userId}</p>
-      <p className="wallet-address wallet-address-header">{props.address}</p>
-    </div>
-  </div>
-);
+class ProfileHeader extends React.Component {
+
+  render() {
+    return (
+      <div className="row align-items-center">
+        <Blockies seed={this.props.address.toLowerCase()} size={8} scale={6} />
+        <div>
+          <p className="user-id user-id-header">{this.props.userId}</p>
+          <p className="wallet-address wallet-address-header">{this.props.address}</p>
+        </div>
+      </div>
+    );
+  }
+
+}
 
 ProfileHeader.propTypes = {
-  userIco: PropTypes.object,
   userId: PropTypes.string,
   address: PropTypes.string
+};
+
+ProfileHeader.defaultProps = {
+  userId: "",
+  address: ""
 };
 
 export default ProfileHeader;
